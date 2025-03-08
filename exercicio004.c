2bc3af32871a04e96657921b4b8a84c22adb6315
Gustavo_Sareto <gustavosareto1@gmail.com>
2025-03-08 08:18:49 -0300

    Add files via upload

---
+++ exercicio004.c
@@ -0,0 +1,14 @@
+/*
+2. Faça um algoritmo que solicite ao usuário um valor e exiba o dobro do valor.
+*/
+
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Informe um valor: ");
+    scanf("%d", &num);
+
+    printf("%d", num * 2);
+}       
